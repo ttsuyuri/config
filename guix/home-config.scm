f9317b1bb69906ca8b4711593edd4ab248859270
tsuyuri <tsuyurii@outlook.com>
2024-01-30 00:11:36 +0800

    migrate config

---
+++ guix/home-config.scm
@@ -0,0 +1,48 @@
+;; This is a sample Guix Home configuration which can help setup your
+;; home directory in the same declarative manner as Guix System.
+;; For more information, see the Home Configuration section of the manual.
+(define-module (guix-home-config)
+  #:use-module (gnu home)
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shells)
+  #:use-module (gnu services)
+  #:use-module (gnu system shadow))
+
+(define home-config
+  (home-environment
+    (services
+      (list
+        ;; Uncomment the shell you wish to use for your user:
+        ;(service home-bash-service-type)
+        ;(service home-fish-service-type)
+        ;(service home-zsh-service-type)
+	(simple-service 'defenv home-environment-variables-service-type
+	   ;;proxy
+	 `(("https_proxy" . "http://127.0.0.1:7890")
+	   ("http_proxy"  . "http://127.0.0.1:7890")
+
+	   ;;wayland
+           ("GDK_BACKEND" . "wayland")
+           ("QT_QPA_PLATFORM" . "wayland")
+           ("QT_QPA_PLATFORMTHEME" . "qt5ct")
+           ("CLUTTER_BACKEND" . "wayland")
+           ("SDL_VIDEODRIVER" . "wayland")
+
+
+           ;;input method
+           ("XMODIFIERS" . "@im=fcitx")
+           ("GTK_IM_MODULE" . "fcitx")
+           ("GLFW_IM_MODULE" . "ibus")
+           ("QT_IM_MODULE" . "fcitx")
+           ("QT_PLUGIN_PATH" . "${HOME}/.guix-profile/lib/qt5/plugins")
+           ("GUIX_GTK3_IM_MODULE_FILE" . "${HOME}/.guix-profile/lib/gtk-3.0/3.0.0/immodules-gtk3.cache")))))))
+
+        ;(service home-files-service-type
+        ; `((".guile" ,%default-dotguile)
+        ;   (".Xdefaults" ,%default-xdefaults)))
+
+        ;(service home-xdg-configuration-files-service-type
+        ; `(("gdb/gdbinit" ,%default-gdbinit)
+        ;   ("nano/nanorc" ,%default-nanorc)))))))
+
+home-config
